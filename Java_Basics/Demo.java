b5285e4c89c67b6a742643dc5f99eaac62df4b04
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-04 15:58:27 +0000

    Basic addation

---
+++ Java_Basics/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[]args){
+         int num = 3;
+         int num1 = 5;
+         int result = num +num1;
+        System.out.println(result);
+    }
+    
+}
