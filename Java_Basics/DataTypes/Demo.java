e4a20f2df912c748f8357f0eee09fe0a98f1cb55
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-04 16:17:23 +0000

    DataTypes  {int float double short long char bool

---
+++ Java_Basics/DataTypes/Demo.java
@@ -0,0 +1,18 @@
+package DataTypes;
+
+public class Demo {
+    public static void main(String[]arga){
+        int num = 9;
+        byte by =127;
+        short s = 558;
+        long l = 4564l;
+        float F = 5.6F;
+         double d = 4.5;
+        char c = 'k';
+        boolean b = true;
+        System.out.println(num + " " +by+ " " +b+ " " +s+ " " +l+ " " +F+ " " +d+ " "  ); 
+
+
+    }
+    
+}
