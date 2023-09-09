8f7cf13116253b5c89b1025f8f06ee6731c180f6
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-09-09 18:16:53 +0000

    switch

---
+++ Java_Basics/Control.java
@@ -0,0 +1,18 @@
+class Control{
+    public static void main(String[]args){
+        int a =  10;
+        switch(a)
+        {
+            case 1 :
+                System.out.println("Deepak");
+                break;
+                case 2:
+                    System.out.println("kumar");
+                    break;
+                    case 3:
+                        System.out.println("Kumar042");
+                        default:
+
+        }
+    }
+}
